f0217b77fe96a858fe9d46ad53b99313066ab6b6
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-12-04 23:35:43 -0500

    stretch done

--- src/main/java/com/lambda/piggybank/controllers/CoinController.java
+++ src/main/java/com/lambda/piggybank/controllers/CoinController.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class CoinController
@@ -44,5 +44,59 @@ public class CoinController
 		sysout += "The piggy bank hold " + df.format(total);
 		System.out.println(sysout);
 		return new ResponseEntity<>(df.format(total), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/money/{amount}", produces = "application/json")
+	public ResponseEntity<?> deleteMoney(@PathVariable double amount)
+	{
+		List<Coin> coinList = new ArrayList<>();
+		coinRepository.findAll().iterator().forEachRemaining(coinList::add);
+		coinList.sort((c1, c2) -> (int) (c2.getValue() * 100 - c1.getValue() * 100));
+		
+		int amtX100 = (int)(amount * 100);
+		StringBuilder deletionOut = new StringBuilder();
+		
+		for (Coin coin : coinList)
+		{
+			if (amtX100 == 0) break;
+			if (amtX100 < (int)(coin.getValue() * 100))
+			{
+				continue;
+			}
+			else if (amtX100 < (int)((coin.getValue() * 100) * coin.getQuantity()))
+			{
+				long cp = amtX100;
+				amtX100 %= coin.getValue() * 100;
+				long quant = coin.getQuantity();
+				long newAmt = quant - ((cp - amtX100) / (long)(coin.getValue() * 100));
+				coin.setQuantity(newAmt);
+				deletionOut.append(quant - newAmt > 1 ? "\nRemoved " + (quant - newAmt) + " " + coin.getNameplural() :
+				"\nRemoved " + (quant - newAmt) + " " + coin.getName());
+			}
+			else
+			{
+				amtX100 -= (int)((coin.getValue() * 100) * coin.getQuantity());
+				long quant = coin.getQuantity();
+				coin.setQuantity(0);
+				deletionOut.append(quant > 1 ? "\nRemoved " + quant + " " + coin.getNameplural() :
+						"\nRemoved " + quant + " " + coin.getName());
+			}
+		}
+		System.out.println(deletionOut);
+		System.out.println("The new piggy bank contents: ");
+		
+		StringBuilder coins = new StringBuilder();
+		for (Coin coin : coinList)
+		{
+			if (coin.getQuantity() > 0)
+			{
+				coins.append(coin.getQuantity() > 1 ? coin.getQuantity() + " " + coin.getNameplural() + "\n" :
+						coin.getQuantity() + " " + coin.getName() + "\n");
+			}
+		}
+		
+		System.out.println(coins);
+		
+		return new ResponseEntity<>(coinList, HttpStatus.OK);
 	}
 }
